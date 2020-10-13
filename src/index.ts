5f65da904c06d48f6991e137e604dfb0094be081
tayamba mwanza <tayambamwanza@gmail.com>
2020-10-13 12:29:04 +0200

    feat: add default values for icon generator

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import jimp from "jimp";
+// import colors from "colors";
+// import fs from "fs";
+
+let defaultIconInput = './icon.png';
+let defaultAssetsOutput = './src/assets/icons';
+let defaultFaviconOutput = './src';
+let defaultSizes = "512, 384, 192, 152, 144, 128, 96, 72";
+let defaultIconName = 'icon-*x*.png';
+let isDryRun = false;
+
+// jimp.read();
